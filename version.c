94ee7b47a7b40849201274c4096a183b6eeeffe5
LuDong <noodle1983@126.com>
2011-03-26 17:04:36 +0800

    pre release 1.0

--- version.c
+++ version.c
@@ -30,7 +30,7 @@ char sshver[] = "PuTTY-Custom-r" STR(SVN_REV);
 
 #else
 
-char ver[] = "Noodle build 0.1 , " __DATE__ " " __TIME__;
+char ver[] = "Noodle build 1.0 beta, " __DATE__ " " __TIME__;
 char sshver[] = "PuTTY-Local: " __DATE__ " " __TIME__;
 
 #endif
