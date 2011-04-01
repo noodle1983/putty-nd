adcb10f551feb265e56452b15f9ac15bb98bfdca
LuDong <noodle1983@126.com>
2011-04-01 17:06:04 +0800

    adjust the dir and release script

--- windows/release.pl
+++ windows/release.pl
@@ -6,5 +6,6 @@ my $version_no = `grep Noodle ../version.c`;
 chomp $version_no;
 $version_no =~ s/^.*?([0-9\.]+).*$/\1/;
 
-print `rm "putty6.0_nd$version_no.zip"`;
-print `zip -9 -v "putty6.0_nd$version_no.zip" *.exe release_note.txt`;
+print `rm "./release/putty6.0_nd$version_no.zip"`;
+print `zip -9 -v "./release/putty6.0_nd$version_no.zip" *.exe release_note.txt`;
+print `cp release_note.txt ./release/readme.txt`;
