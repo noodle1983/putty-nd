5a6674346df401233122be7a2c660be786c93612
LuDong <noodle1983@126.com>
2011-04-30 11:32:28 +0800

    release putty6.0_nd1.6

--- version.c
+++ version.c
@@ -30,7 +30,7 @@ char sshver[] = "PuTTY-Custom-r" STR(SVN_REV);
 
 #else
 
-char ver[] = "Noodle build 1.5, " __DATE__ " " __TIME__;
+char ver[] = "Noodle build 1.6, " __DATE__ " " __TIME__;
 char sshver[] = "PuTTY-Local: " __DATE__ " " __TIME__;
 
 #endif
