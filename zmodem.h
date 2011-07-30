23b966b5914962bdf03eb6d7d00eeb20ace5b8fd
LuDong <noodle1983@126.com>
2011-07-30 11:45:52 +0800

    add two files

---
+++ zmodem.h
@@ -0,0 +1,30 @@
+#ifndef ZMODEM_H
+#define ZMODEM_H
+
+// Frame types
+#define ZRQINIT    0
+#define ZRINIT     1
+#define ZSINIT     2
+#define ZACK       3
+#define ZFILE      4
+#define ZSKIP      5
+#define ZNAK       6
+#define ZABORT     7
+#define ZFIN       8
+#define ZRPOS      9
+#define ZDATA      10
+#define ZEOF       11
+#define ZFERR      12
+#define ZCRC       13
+#define ZCHALLENGE 14
+#define ZCOMPL     15
+#define ZCAN       16
+#define ZFREECNT   17
+#define ZCOMMAND   18
+#define ZSTDERR    19
+
+
+
+
+#endif /* ZMODEM_H */
+
